696d7b84d256b4970463727dfd5f6a4f93443a61
Bozidar Milivojevic <bozidarmilivojevic@live.com>
2019-04-01 10:15:59 +0200

    Add decorator for tags, regex validation for tag name

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,12 @@ module FileManagementApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.to_prepare do
+      # Load application's model / class decorators
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
