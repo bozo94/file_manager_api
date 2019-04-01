d7d81477eb3df67bbf6470e81f0ac9810383e921
Bozidar Milivojevic <bozidarmilivojevic@live.com>
2019-04-01 21:16:50 +0200

    Minnor fix

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -1,10 +1,10 @@
 module Response
   def json_response(object, status = :ok)
-    if(object.kind_of?(Array))
+    if(object.kind_of?(ActiveRecord::Relation))
       related_tags = []
       object.each do |obj|
         obj.tags.each do |tag|
-          related_tags << {tag: tag.name, file_count: tag.taggings_count} unless @select_tags.include? tag.name
+          related_tags << {tag: tag.name, file_count: tag.taggings_count} unless @select_tags and @select_tags.include? tag.name
         end
       end
       render json: object,
