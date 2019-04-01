0269ceb2ccb0710780449ad9b263ed374b473ac9
Bozidar Milivojevic <bozidarmilivojevic@live.com>
2019-04-01 19:07:13 +0200

    Add total records to response

--- app/controllers/api/v1/files_controller.rb
+++ app/controllers/api/v1/files_controller.rb
@@ -7,10 +7,9 @@ module Api
         params[:page] ||= 1
         if params[:tag_search_query]
           tags = params[:tag_search_query].split(" ")
-          select_tags = tags.select{|tag| tag[0]=='+'}.map{|tag| tag[1..-1]}
-          deselect_tags = tags.select{|tag| tag[0]=='-'}.map{|tag| tag[1..-1]}
-          debugger
-          @api_files = @api_files.tagged_with(select_tags, :match_all => false) - @api_files.tagged_with(deselect_tags, :match_all => false) 
+          @select_tags = tags.select{|tag| tag[0]=='+'}.map{|tag| tag[1..-1]}
+          @deselect_tags = tags.select{|tag| tag[0]=='-'}.map{|tag| tag[1..-1]}
+          @api_files = @api_files.tagged_with(@select_tags, :match_all => false) - @api_files.tagged_with(@deselect_tags, :match_all => false) 
         else
           @api_files = @api_files.page params[:page]
         end
--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -1,5 +1,11 @@
 module Response
   def json_response(object, status = :ok)
-    render json: object, status: status
+    if(object.kind_of?(Array))
+      render json: object,
+             status: status,
+             total_records: object.count
+    else
+      render json: object, status: status
+    end
   end
 end
